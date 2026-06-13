0cf180ee91f2af00ea721e2e1455940e87796399
Dustin <xatrekak@gmail.com>
2026-06-13 12:41:18 -0400

    Normalize variable naming for socket address
    Switch variable to camelCase

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -20,12 +20,12 @@ export const checkUser: Middleware = async (req, _res, next) => {
   // Check if the remoteSocketAddress we received the request
   // from is trusted!
   const socketAddress = req.socket.remoteAddress || '';
-  const ipv4_normalized_socketAddress = socketAddress.replace(/^::ffff:/, '');
+  const ipv4NormalizedSocketAddress = socketAddress.replace(/^::ffff:/, '');
 
-  if (net.isIPv4(ipv4_normalized_socketAddress)) {
+  if (net.isIPv4(ipv4NormalizedSocketAddress)) {
     trustedProxy =
-      ipv4_normalized_socketAddress === '127.0.0.1' ||
-      settings.network.trustedProxies.v4.includes(ipv4_normalized_socketAddress);
+      ipv4NormalizedSocketAddress === '127.0.0.1' ||
+      settings.network.trustedProxies.v4.includes(ipv4NormalizedSocketAddress);
   } else if (net.isIPv6(socketAddress)) {
     trustedProxy =
       socketAddress === '::1' ||
